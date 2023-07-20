b3b2e4398e9a61aab8054eabb521227341e0e5c4
emjshrx <emjshrx@gmail.com>
2023-07-20 23:06:06 +0530

    mostly done

--- exercises/week 1/solutions/emjshrx.sh
+++ exercises/week 1/solutions/emjshrx.sh
@@ -10,49 +10,58 @@ echo "1. Initiate "
 echo "2. Usage "
 echo "3. Exit "
 
-read -p "Option : " option
+read -n 1 -p "Option : " option
 case $option in 
 0 )
-    mkdir install
+    #mkdir install
     cd install
-    wget https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz
-    wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
-    sha256sum --ignore-missing --check SHA256SUMS
-    # add a grep here to verify
-    wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS.asc
+    #wget https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz
+    #wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
+    if sha256sum --ignore-missing --check SHA256SUMS | grep -q 'bitcoin-25.0-x86_64-linux-gnu.tar.gz: OK'; then
+    echo "Binary signature verification successful"
+    fi
+    #wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS.asc
     git clone https://github.com/bitcoin-core/guix.sigs
     gpg --import guix.sigs/builder-keys/*
     gpg --verify SHA256SUMS.asc
-    # add a grep here as well
-
-    tar --extract bitcoin-25.0-x86_64-linux-gnu.tar.gz
-    cp bitcoin-25.0-x86_64-linux-gnu /usr/local/bin/
+    tar --extract -f bitcoin-25.0-x86_64-linux-gnu.tar.gz
+    echo "Done"
+    #cp bitcoin-25.0 /usr/local/bin/
     ;;
 1 )
-    echo "regtest=1" >> ~/.bitcoin/bitcoin.conf
-    echo "fallbackfee=0.0001" >> ~/.bitcoin/bitcoin.conf
-    bitcioind
+    rm ~/.bitcoin/bitcoin.conf
+    echo "regtest=1
+fallbackfee=0.0001
+server=1
+txindex=1" >> ~/.bitcoin/bitcoin.conf
+    bitcoind -daemon
+    sleep 5
     bitcoin-cli createwallet "Miner"
     bitcoin-cli createwallet "Trader"
     bitcoin-cli loadwallet Miner
-    # generate address with label and save to var
-    bitcoin-cli generatetoaddress 101 bcrt1qna46vgw2gavgvqxgrpss4phy7cxp8t30walvc9
+    mineraddr=$(bitcoin-cli -rpcwallet=Miner getnewaddress "Mining Reward")
+    bitcoin-cli generatetoaddress 101 $mineraddr
     # coinbase transactions are locked for 100 blocks to accomadate reorgs in this time
     bitcoin-cli getbalance
     ;;
 2 )
     bitcoin-cli loadwallet Trader
-    txid=$(bitcoin-cli sendtoaddress n2eMqTT929pb1RDNuqEnxdaLau1rxy3efi 20)
-    bitcoin-cli getmempoolentry $txid
-    bitcoin-cli generatetoaddress 1 bcrt1qna46vgw2gavgvqxgrpss4phy7cxp8t30walvc9
-    txdet = $(bitcoin-cli gettransaction $txid false true)
+    traderaddr=$(bitcoin-cli -rpcwallet=Trader getnewaddress "Received")
+    txid=$(bitcoin-cli -rpcwallet=Miner sendtoaddress $traderaddr 20)
+    bitcoin-cli -rpcwallet=Miner getmempoolentry $txid
+    bitcoin-cli -rpcwallet=Miner generatetoaddress 1 $mineraddr
+    txdet=$(bitcoin-cli -rpcwallet=Miner gettransaction $txid false true)
     echo "txid : " $txid
-    echo $txdet | jq ".details[0].address"
+    echo $txdet | jq "."
+    ;;
 
-;;
-3 ) exit
+3 ) 
+    bitcoin-cli stop
+    exit
     ;;
 * ) 
-    echo " Sorry invalid option"
+    echo "Sorry invalid option"
 esac
+read -n 1 -s -r -p "Press any key to continue"
+
 done
