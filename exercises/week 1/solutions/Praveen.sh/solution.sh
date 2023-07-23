71e25d270eadf2fbe21b4af84fd687e41d6dd07a
Praveen <spraveenitpro@gmail.com>
2023-07-22 22:49:16 -0700

    Update script to install bitcoin core on mac and create conf file

--- exercises/week 1/solutions/Praveen.sh/solution.sh
+++ exercises/week 1/solutions/Praveen.sh/solution.sh
@@ -15,7 +15,7 @@ wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
 
 # Verify the hash of the binary file.
 
-shasum --check SHA256SUMS | grep OK > /dev/null 2>&1
+shasum --check SHA256SUMS 2>/dev/null | grep OK
 
 #If successful verification then printmessage to terminal that Binary hash verification successful"
 
@@ -27,7 +27,7 @@ fi
 
 # Verify the signature of the hash file.
 
-gpg --verify SHA256SUMS.asc SHA256SUMS > /dev/null 2>&1
+gpg --verify SHA256SUMS.asc SHA256SUMS 2>/dev/null
 
 # write a if condition to check if the signature is verified or not
 
@@ -36,4 +36,23 @@ if [ $? -eq 0 ]; then
 else
     echo "Signature verification failed"
 fi
+
+# install the above binary and move the binaries to /usr/local/bin/ for folder.
+
+sudo hdiutil attach bitcoin-25.0-arm64-apple-darwin.dmg
+sudo cp -r /Volumes/Bitcoin-Core/bitcoin-25.0/* /usr/local/bin/
+sudo chown -R $USER /usr/local/bin/bitcoin*
+sudo chmod 755 /usr/local/bin/bitcoin*
+
+# Create a bitcoin.conf file in the /Users/$USER/Library/Application Support/Bitcoin and add "regtest=1" to it:
+
+mkdir -p /Users/$USER/Library/Application\ Support/Bitcoin
+echo "regtest=1" >> /Users/$USER/Library/Application\ Support/Bitcoin/bitcoin.conf
+echo "fallbackfee=0.0001" >> /Users/$USER/Library/Application\ Support/Bitcoin/bitcoin.conf
+echo "server=1" >> /Users/$USER/Library/Application\ Support/Bitcoin/bitcoin.conf
+echo "txindex=1" >> /Users/$USER/Library/Application\ Support/Bitcoin/bitcoin.conf
+
+
+
+
 
