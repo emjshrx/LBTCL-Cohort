0b96c1439934b187f9d8c74fa06903c4ca3ddfa9
Praveen <spraveenitpro@gmail.com>
2023-07-24 14:48:17 -0700

    Update script with addtional logic for mac

--- exercises/week 1/solutions/Praveen.sh/solution.sh
+++ exercises/week 1/solutions/Praveen.sh/solution.sh
@@ -5,6 +5,7 @@ echo "Let there be a node"
 
 # Function to download the binary, hash and signature files
 download_binary() {
+    echo "Downloading the binary, hash and signature files"
     wget https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-arm64-apple-darwin.dmg
     wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS.asc
     wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
@@ -12,6 +13,7 @@ download_binary() {
 
 # Function to verify the hash of the binary file
 verify_binary_hash() {
+    echo "Verifying the hash of the binary file"
     shasum --check SHA256SUMS 2>/dev/null | grep OK
 
     # If successful verification then print message to terminal that Binary hash verification successful"
@@ -24,6 +26,7 @@ verify_binary_hash() {
 
 # Function to verify the signature of the hash file
 verify_signature() {
+    echo "Verifying the signature of the hash file"
     gpg --verify SHA256SUMS.asc SHA256SUMS 2>/dev/null
 
     # Write an if condition to check if the signature is verified or not
@@ -36,6 +39,7 @@ verify_signature() {
 
 # Function to check if bitcoin core is installed via homebrew casks, if not install it
 check_installation() {
+    echo "Checking if bitcoin core is installed on your mac"
     brew ls | grep "bitcoin-core"
 
     if [ $? -eq 0 ]; then
@@ -48,6 +52,7 @@ check_installation() {
 
 # Function to create a bitcoin.conf file in the /Users/$USER/Library/Application Support/Bitcoin
 create_conf_file() {
+    echo "Creating bitcoin.conf file"
     cd /Users/$USER/Library/Application\ Support/Bitcoin
 
     # Create a file called bitcoin.conf
@@ -59,8 +64,19 @@ create_conf_file() {
     echo "txindex=1" >> bitcoin.conf
 }
 
+# Function to delete regtest dir if already exists within /Users/$USER/Library/Application\ Support/Bitcoin
+delete_regtest_dir() {
+    echo "Deleting regtest directory if exists"
+    if [ -d "/Users/$USER/Library/Application\ Support/Bitcoin/regtest" ]; then
+        rm -rf /Users/$USER/Library/Application\ Support/Bitcoin/regtest
+    fi
+}
+
+
+
 # Function to start bitcoind in the background and run the last command
 start_bitcoind() {
+    echo "Starting bitcoind"
     # Start bitcoind in the background
     bitcoind -daemon
 
@@ -74,6 +90,7 @@ start_bitcoind() {
 # Function to create 2 wallets called Miner and Trader
 
 create_wallets() {
+    echo "Creating two wallets"
     # Create a wallet called Miner
     bitcoin-cli createwallet "Miner"
 
@@ -88,6 +105,7 @@ generate_address_and_mine_blocks() {
     #  In regtest mode, the block generation time is significantly reduced to allow for faster testing
     #  and development. By generating 101 blocks, a sufficient number of confirmations is achieved, and
     #  the network considers the transactions confirmed and reflects the updated wallet balance.
+    echo "Generate an address for miner and mine blocks"
     miner_address=$(bitcoin-cli -rpcwallet="Miner" getnewaddress "Mining Reward")
     bitcoin-cli -rpcwallet="Miner" generatetoaddress 101 $miner_address
     bitcoin-cli -rpcwallet="Miner" getbalance
@@ -98,27 +116,29 @@ generate_address_and_mine_blocks() {
 # print the transaction id and check the mempool for the transaction
 
 generate_trader_address() {
+    echo "Generate an address for trader"
     amount=20
     trader_address=$(bitcoin-cli -rpcwallet="Trader" getnewaddress "Received")
-    tx_id=bitcoin-cli -rpcwallet="Miner" sendtoaddress $trader_address $20
+    tx_id=$(bitcoin-cli -rpcwallet="Miner" sendtoaddress $trader_address $amount)
     echo "Transaction ID: $tx_id"
-    bc getmempoolentry $tx_id
+    bitcoin-cli getmempoolentry $tx_id
 }
 
 # Function to Confirm the transaction by creating 1 more block.
 
 confirm_block() {
-    bitcoin-cli -rpcwallet="Miner" generatetoaddress 1 $miner_address
+    echo "Generating one additional block"
+    bitcoin-cli -rpcwallet="Miner" -generate 1
     bitcoin-cli -rpcwallet="Miner" getbalance
 }
 
 # Function to display transaction details
 
 display_transaction_details() {
-    # bitcoin-cli -rpcwallet="Trader" gettransaction $tx_id
-    bitcoin-cli getrawtransaction "${tx_id}" true
-    fee=$(bitcoin-cli -rpcwallet="first_wallet" gettransaction $tx_id | jq '.fee')
-    blockheight=blockheight=$(bitcoin-cli -rpcwallet="first_wallet" gettransaction $tx_id | jq '.blockheight')
+    echo "Display transaction details"
+    bitcoin-cli -rpcwallet="Miner"  gettransaction "${tx_id}" true
+    fee=$(bitcoin-cli -rpcwallet="Miner" gettransaction $tx_id | jq '.fee')
+    blockheight=blockheight=$(bitcoin-cli -rpcwallet="Miner" gettransaction $tx_id | jq '.blockheight')
     echo "Transaction ID: $tx_id"
     echo "<From, Amount>: ${miner_address}, ${amount}"
 	echo "<Send, Amount>: ${trader_address}, ${amount}"
@@ -139,12 +159,13 @@ download_binary
 verify_binary_hash
 verify_signature
 check_installation
+delete_regtest_dir
 create_conf_file
 start_bitcoind
 create_wallets
-generate_address
-mine_blocks
 generate_address_and_mine_blocks
+generate_trader_address
+confirm_block
 display_transaction_details
 
 
